b418cdd677e156923de6f021da3bbb9abde3f2f0
drivanreis <105787691+drivanreis@users.noreply.github.com>
2023-09-09 11:23:41 -0300

    Finalizado

---
+++ src/helpers/weatherAPI.js
@@ -0,0 +1,39 @@
+// Importando o Token para usar como chave da API
+const TOKEN = import.meta.env.VITE_TOKEN;
+
+export const searchCities = async (nomeCid) => {
+  try {
+    // Verrifica se foi digitado o nome da cidade
+    if (nomeCid.length === 0) {
+      window.alert('Digite o nome da cidade');
+    }
+
+    const resposta = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${nomeCid}&aqi=no`);
+    const dados = await resposta.json();
+    // Verrifica se os dados foram encontrados
+    if (dados.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+    }
+
+    // Retorno os dados encontrados
+    return dados;
+    // E se acontecer algum erro,
+  } catch (err) {
+    // Passo o erro para o alert colocar na tela.
+    window.alert(err.message);
+  }
+};
+
+export const getWeatherByCity = async (urlCid) => {
+  // Solicitando dados da API
+  const resultado = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${urlCid}`);
+  // Passando os dados para o formato json
+  const dados = await resultado.json();
+  // Descontruindo os dados em um formato mais simples
+  const { current: { temp_c: tempC, condition: { text, icon } } } = dados;
+  return {
+    temp: tempC,
+    condition: text,
+    icon,
+  };
+};
